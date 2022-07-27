4fd529fc817c30794fafe9b23af422a0c17067bd
VCoficial <ciberandresoficial@gmail.com>
2022-07-27 17:38:18 -0500

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,12 @@
+//Activar Plantillas
+$(function(){
+    $("#footer").load("assets/inc/footer.html"); 
+    $("#secciones").load("assets/inc/cabecera.html"); 
+});
+
+//Primera Actividad
+loadliveworksheet(1406916, 'xrfilkvd', 1413, 'es', 4031835);
+//Segunda Actividad
+loadliveworksheet(1402964, 'oovpfpbe', 1413, 'es', 4031835);
+//Tercera Actividad
+loadliveworksheet(1401925, 'kbeftapc', 1415, 'es', 4031835);
